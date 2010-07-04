5d9ec1cd1d55cebaae4a6aba290c09df3909cd6d
Grzegorz Anielak <g.anielak@gmail.com>
2010-07-04 16:15:47 +0200

    Turn off everything.

--- implementation/switches.cpp
+++ implementation/switches.cpp
@@ -4,10 +4,10 @@ namespace Hex {
 
 SWITCH_TYPE Switches::hacksOn = true;
 SWITCH_TYPE Switches::avoidingBridgesOn = false;
-SWITCH_TYPE Switches::defendingBridgesOn = true;
+SWITCH_TYPE Switches::defendingBridgesOn = false;
 SWITCH_TYPE Switches::patternsOn = false;
-SWITCH_TYPE Switches::detectWins = true;
-SWITCH_TYPE Switches::raveOn = true;
+SWITCH_TYPE Switches::detectWins = false;
+SWITCH_TYPE Switches::raveOn = false;
 SWITCH_TYPE Switches::pathAmafOn = false;
 SWITCH_TYPE Switches::pathRaveOn = false;
 SWITCH_TYPE Switches::floodFillOn = false;
